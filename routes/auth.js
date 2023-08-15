0df2df360c98580e71886d288dc07d8e998ff4b9
vaxad <varadprabhu111@gmail.com>
2023-08-15 23:57:50 +0530

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,9 @@ const router = express.Router()
 router.post('/',(req,res)=>{
     const {pass}=req.body
     if(pass==="admin0511"){
-        res.send(true)
+        res.json({auth:true})
     }else{
-        res.send(false)
+        res.json({auth:false})
     }
 })
 
